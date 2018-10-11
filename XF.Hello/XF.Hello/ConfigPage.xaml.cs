1917340b40af7130d9bc3fd31bea99e79e3a20f8
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-10 22:35:20 -0300

    Add project files.

---
+++ XF.Hello/XF.Hello/ConfigPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.Hello
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ConfigPage : ContentPage
+	{
+		public ConfigPage ()
+		{
+            //Configuracao configuracao = InitConfiguracao(true, false, true, false, true);
+            BindingContext = App.ConfigModel;
+			InitializeComponent ();
+		}
+
+        private Configuracao InitConfiguracao(bool rastrearLocalizacao, bool permitirCookies, bool disponibilizarMural, bool EnviarEmail, bool receberSMS)
+        {
+            return new Configuracao
+            {
+                RastrearLocalizacao = rastrearLocalizacao,
+                PermitirCookies = permitirCookies,
+                DisponibilizarMural = disponibilizarMural,
+                EnviarEmail = EnviarEmail,
+                ReceberSMS = receberSMS
+            };
+            
+        }
+    }
+}
---
+++ XF.Hello/XF.Hello/Configuracao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XF.Hello
+{
+    public class Configuracao
+    {
+        public bool RastrearLocalizacao { get; set; }
+        public bool PermitirCookies { get; set; }
+        public bool DisponibilizarMural { get; set; }
+        public bool EnviarEmail { get; set; }
+        public bool ReceberSMS { get; set; }
+
+    }
+}
